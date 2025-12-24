03f497d54061062dd799dd110f2098b0e430005c
Justin Liu <25liujustin1@gmail.com>
2025-12-24 01:31:37 -0800

    limited countries list

--- public/client.js
+++ public/client.js
@@ -1,44 +1,344 @@
 //intializes client side socket that communicates with server socket via shared socket id
 const socket = io();
-let currentRoom = null; // which room this player is in
+let currentRoomcode = null; // which room this player is in
 let username = null;
-
+let host = false;
+let currentCountry = null;
+/*const countries = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cape Verde",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Comoros",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Democratic Republic of the Congo",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Fiji",
+  "Finland",
+  "France",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kosovo",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "North Korea",
+  "North Macedonia",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Qatar",
+  "Republic of the Congo",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Vatican City",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe"
+];
+*/
+const countryIds = ["libya", "tunisia", "egypt", "afghanistan","albania","algeria","andorra","israel","palestine", "france"];
 //we call this function in html, and this function calls socket in server.js
 
-function setUsername(){
+function toId(name) {
+  return name
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)/g, "");
+}
+
+function intializeCountryEventListeners(){
+
+  for(const countryId of countryIds){
+    document.getElementById(countryId).addEventListener("click", function(){
+      if(toId(currentCountry) === countryId){
+        document.getElementById(countryId).style.fill = "#006B0D";
+        document.getElementById(countryId).style.border = "none";
+        getNewCountry(currentRoomcode);
+      }else if(currentCountry){
+        document.getElementById(countryId).style.fill = "rgba(255, 0, 0, 0.5)";
+        setTimeout(function () {
+        document.getElementById(countryId).style.removeProperty("fill");
+      }, 750);
+      }
+    });
+  }
+}
+
+/*
+
+function intializeCountryEventListeners(){
+
+    document.getElementById("libya").addEventListener("click", function(){
+      if(currentCountry === "libya"){
+        getNewCountry(currentRoomcode);
+        document.getElementById("libya").style.fill = "#006B0D";
+        document.getElementById("libya").style.border = "none";
+      }else if(currentCountry){
+        document.getElementById("libya").style.fill = "rgba(255, 0, 0, 0.5)";
+        setTimeout(function () {
+        document.getElementById("libya").style.removeProperty("fill");
+      }, 750);
+      }
+  });
+}
+*/
+
+
+function randomChars(){
+  let x = Math.floor(Math.random()*58) + 75;
+  if(x > 122){
+    return x - 123;
+  }else{
+    return String.fromCharCode(x);
+  }
+}
+
+function roomCodeGenerator(){
+  let x = "";
+  for (let i = 0; i < 6; i++){
+    x += randomChars();
+  }
+  return x;
+}
+
+function createRoom(){
+  document.getElementById("mainPage").style.display = "none";
+  document.getElementById("roomsPopup").style.display = "none";
+  document.getElementById("startGameButton").style.display = "block";
+  roomcode = roomCodeGenerator();
+  currentRoomcode = roomcode;
+  socket.emit("create-room", roomcode);
+  socket.emit("update-roomcodes", roomcode);
+  host = true;
+  joinRoom(false);
+} 
+
+function joinRoomPrep(){
+  document.getElementById("mainPage").style.display = "none";
+  document.getElementById("roomsPopup").style.display = "none";
+  document.getElementById("joinRoomDisplay").style.display = "block";
+}
+
+function roomsPopup(){
   username = document.getElementById("usernameInput").value.trim();
   if(!username){
     alert("Please enter an username");
     return;
   }
-
-  document.getElementById("usernamePage").style.display = "none";
-  document.getElementById("lobbyView").style.display = "block";
+  document.getElementById("roomsPopup").style.display = "block";
 }
 
-function lobbiesPopup(){
-  document.getElementById("lobbiesPopup").style.display = "block";
+function attemptJoinRoom(attemptRoomcode){
+  socket.emit("roomcode-validation", attemptRoomcode, function(bool){
+    if(bool){
+      joinRoom(attemptRoomcode);
+    }else{
+      alert("Invalid roomcode");
+    }
+  })
 }
 
-function joinRoom() {
-  const room = document.getElementById("roomInput").value.trim();
-  if (!room) {
-    alert("Enter a room code first");
-    return;
+function getNewCountry(){
+  socket.emit("request-country", currentRoomcode);
+}
+
+socket.on("new-country", function(country){
+  if(currentCountry){
+    document.getElementById(toId(currentCountry)).style.fill = "#006B0D";
   }
+  currentCountry = country;
+  document.getElementById("country").textContent = currentCountry;
+});
+
+
+function joinRoom(roomcode) {
 
   // tell server we want to join
-  socket.emit("join-room", room);
-
-  //locally remember the room
-  currentRoom = room;
+  if(roomcode){
+    socket.emit("join-room", roomcode);
+    currentRoomcode = roomcode;
+  }
 
   // switch UI: hide lobby, show room
-  document.getElementById("lobbyView").style.display = "none";
+  document.getElementById("joinRoomDisplay").style.display = "none";
   document.getElementById("roomView").style.display = "block";
-  document.getElementById("roomName").textContent = room;
+  document.getElementById("map").style.display = "block";
+  document.getElementById("roomName").textContent = currentRoomcode;
 
-  appendLog("You joined room: " + room);
+  appendLog("You joined room: " + currentRoomcode);
+
+  intializeCountryEventListeners();
+}
+
+function startGame() {
+  document.getElementById("startGameButton").style.display = "none";
+  getNewCountry();
 }
 
 function sendMsg() {
--- server.js
+++ server.js
@@ -22,6 +22,247 @@ const server = http.createServer(app);
 //constructs io as socket server object
 const io = new Server(server);
 
+class Room{
+
+  constructor(roomcode, host){
+    this.roomcode = roomcode;
+    this.host = host;
+  }
+  /*
+  countries = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cape Verde",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Comoros",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Democratic Republic of the Congo",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Fiji",
+  "Finland",
+  "France",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kosovo",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "North Korea",
+  "North Macedonia",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Qatar",
+  "Republic of the Congo",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Vatican City",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe"
+  ];
+  */
+  countries = ["Libya", "Tunisia", "Egypt", "Afghanistan","Albania","Algeria","Andorra","Israel","Palestine"];
+
+  players = [];
+
+}
+
+let rooms = [];
+
+let roomcodes = [];
+
+function isRoom(input){
+  for(const roomCode of roomcodes){
+    if(input == roomCode){
+      return true;
+    }
+  }
+  return false;
+}
+
+function getRoom(Roomcode){
+  for(const room of rooms){
+    if(room.roomcode === Roomcode){
+      return room;
+    }
+  }
+}
+
+
+function getRandomCountry(room){
+  let x = Math.floor(Math.random()*room.countries.length);
+  let temp = room.countries[x];
+  room.countries.splice(x, 1);
+  return temp;
+}
+
+
 //---------Mongoose Connection---------
 
 //gets the url of my mongodb
@@ -54,17 +295,43 @@ app.use(express.static("public"));
 io.on("connection", function(socket) {
   console.log("Player connected:", socket.id);
   
-  //if connection made, begin scanning for socket events
+  //if connection made, begin scanning for socket events4
+
+  socket.on("request-country", function(roomcode){
+    country = getRandomCountry(getRoom(roomcode));
+    io.to(roomcode).emit("new-country", country);
+  });
+  
+  socket.on("roomcode-validation", function(attemptRoomcode, callback){
+    callback(isRoom(attemptRoomcode));
+  });
+
+  socket.on("update-roomcodes", function(roomcode){
+    roomcodes.push(roomcode);
+  });
+
+  socket.on("create-room", function(roomcode){
+    const room = new Room(roomcode, socket.id);
+    rooms.push(room);
+    roomcodes.push(roomcode);
+    socket.join(roomcode);
+    console.log(`Player ${socket.id} joined room ${roomcode}`);
+    io.to(roomcode).emit(
+      "system-msg",
+      `Player ${socket.id} joined room ${roomcode}`
+    );
+  });
 
   //if user tries to join room, then let them into room and send messages
-  socket.on("join-room", function(room) {
-    room = (room || "").trim();
-    if (!room) return;
-    socket.join(room);
-    console.log(`Player ${socket.id} joined room ${room}`);
-    io.to(room).emit(
+  socket.on("join-room", function(roomcode) {
+    roomcode = (roomcode || "").trim();
+    if (!roomcode) return;
+    socket.join(roomcode);
+    getRoom(roomcode).players.push(socket.id);
+    console.log(`Player ${socket.id} joined room ${roomcode}`);
+    io.to(roomcode).emit(
       "system-msg",
-      `Player ${socket.id} joined room ${room}`
+      `Player ${socket.id} joined room ${roomcode}`
     );
   });
   
@@ -83,6 +350,8 @@ io.on("connection", function(socket) {
   socket.on("disconnect", function() {
     console.log("Player disconnected:", socket.id);
   });
+
+
 });
 
 
